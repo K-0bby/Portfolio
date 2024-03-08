329a6b10e5da90abe030b75c716633e9c54d6d3b
K-0bby <k.hoenyefia@gmail.com>
2024-03-08 22:23:35 +0000

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Link } from 'react-scroll';
-import me from '../Assets/images/me.jpg';
+// import { Link } from 'react-scroll';
+// import me from '../Assets/images/me.jpg';
+import kobby from '../Assets/images/me(2).png'
 import SocialMedia from './SocialMedia';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDownload } from '@fortawesome/free-solid-svg-icons';
@@ -11,18 +12,19 @@ const Home = () => {
     <section className='homePage' id='home'>
       <div className="home-container">
         <h2>Hi 👋, I am <span>Gideon Korbla Hoenyefia</span></h2>
-        <img src={me} alt="Gideon" />
-        <h3>Frontend Developer</h3>
-        <SocialMedia />
+        <h3>A Frontend Developer</h3>
         <p>My expertise is creating contemporary and engaging websites and web apps that 
           enhance customer engagement through user-friendly designs that combine
           style and functionality.
         </p>
+        <SocialMedia />
         <div className="buttons">
           <a href="https://drive.google.com/file/d/1m-IAv2yghKPPmqCUek8dCiYUp2NcdV8b/view?usp=sharing" target='_blank' rel='noreferrer' className="h-button">Download CV <FontAwesomeIcon icon={faDownload} /></a>
-          <Link to="contact" spy={true} smooth={true} offset={-100} duration={500} className="home-button">Hire Me</Link>
+          {/* <Link to="contact" spy={true} smooth={true} offset={-100} duration={500} className="home-button">Hire Me</Link> */}
         </div>
-      </div> 
+      </div>
+      <img src={kobby} alt="Gideon" />
+      
     </section>
   )
 }
